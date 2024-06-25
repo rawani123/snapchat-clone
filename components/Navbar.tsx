d73c83098ee4ad56566ff033afa2a1eb3a5d51ed
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-25 16:14:59 +0530

    setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between w-screen px-10 py-4'>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar
