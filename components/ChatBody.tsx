347c7ee5d5af17c395d87aa00cd8b15f22a38f1e
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-30 20:36:04 +0530

    chat body done

--- components/ChatBody.tsx
+++ components/ChatBody.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const ChatBody = () => {
   return (
-    <div>
-      ChatBody
+    <div className='flex-1 my-3 border-2 border-gray-200 overflow-y-auto p2 rounded-lg'>
+        chatBody
+
     </div>
   )
 }
