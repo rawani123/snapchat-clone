ac12e603951fdf3abb6d604e20156e323c3e7575
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-30 20:45:22 +0530

    input boc done

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { MdPhotoCamera } from "react-icons/md";
+import { Button } from "./ui/button";
 
 const ChatInput = () => {
   return (
-    <div>
-      ChatInput
+    <div className="flex items-center justify-between gap-2">
+      <div className="p-2 cursor-pointer bg-gray-200 rounded-full">
+        <MdPhotoCamera size={"24px"} />
+      </div>
+      <form action="" className="w-full">
+        <div className="flex items-center justify-center gap-4">
+          <input
+            type="text"
+            placeholder="Send a snap message"
+            className="rounded-full w-full border border-gray-400 p-2 outline-none font-medium"
+          />
+          <Button type="submit" className="rounded-full">Send snap</Button>
+        </div>
+      </form>
+      <div>
+        Emoji
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatInput
+export default ChatInput;
