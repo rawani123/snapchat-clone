1c2d075bc529c34a17d922a0a5b297cc43c52df2
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-07-01 09:55:49 +0530

    create message done

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+"use client"
+import React,{useState} from "react";
 import { MdPhotoCamera } from "react-icons/md";
 import { Button } from "./ui/button";
 import { PopoverDemo } from "./EmojiPopover";
 
 const ChatInput = () => {
+  const [input, setInput] = useState<string>("");
+
+  const onSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    try {
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="flex items-center justify-between gap-2">
       <div className="p-2 cursor-pointer bg-gray-200 rounded-full">
@@ -12,6 +24,8 @@ const ChatInput = () => {
       <form action="" className="w-full">
         <div className="flex items-center justify-center gap-4">
           <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             type="text"
             placeholder="Send a snap message"
             className="rounded-full w-full border border-gray-400 p-2 outline-none font-medium"
--- lib/serveractions.ts
+++ lib/serveractions.ts
@@ -1,6 +1,38 @@
 'use server'
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Chat } from "@/models/chat.model";
+import Message from "@/models/message.model";
 import { redirect } from "next/navigation";
+
+export const sendSnap =async(image:string,receiverId:string,messageType:'image'|'text')=>{
+    try {
+        const authUser=await auth()
+        const senderId=authUser?.user?._id;
+        const newMessage= await Message.create({
+            senderId,
+            receiverId,
+            content:"img url",
+            messageType
+        })
+
+        let chat=await Chat.findOne({participants:{$all:[senderId,receiverId]}})
+        if(!chat){
+            chat=await Chat.create({
+                participants:[senderId,receiverId],
+                messages:[newMessage._id]
+            })
+        }else{
+            chat.messages.push(newMessage._id)
+            await chat.save()
+        }
+
+        return JSON.parse(JSON.stringify(newMessage))
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
 
 export const logoutHandler = async () => {
     'use server'
