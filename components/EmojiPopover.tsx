98ed299fc62e12da2dc5a57ebe304bbee9d6527b
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-07-02 10:42:23 +0530

    send emoji done

--- components/EmojiPopover.tsx
+++ components/EmojiPopover.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import {
   Popover,
@@ -5,10 +7,33 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { EmojiArray } from "@/lib/emoji"
+import { sendSnap } from "@/lib/serveractions"
+import { readFileAsDataURL } from "@/lib/utils"
 import Image from "next/image"
+import { useParams } from "next/navigation"
+import { useState } from "react"
 import { MdEmojiEmotions } from "react-icons/md"
 
 export function PopoverDemo() {
+  const [loading ,setLoading]=useState(false)
+  const {id}= useParams<{id:string}>()
+
+  const handleSendEmoji=async (srcURl:string)=>{
+    try {
+      setLoading(false)
+      const blob = await fetch(srcURl).then((res:any)=>{
+        res.blob()
+      })
+      const dataUrl = await readFileAsDataURL(blob!)
+      await sendSnap(dataUrl,id,"image")
+    } catch (error) {
+      console.log(error)
+    }
+    finally{
+      setLoading(false)
+    }
+  }
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -21,7 +46,7 @@ export function PopoverDemo() {
         {
             EmojiArray.map((emoji,index)=>{
                 return (
-                   <div key={index}  className="cursor-pointer scale-90 hover:scale-110 transition-transform duration-100">
+                   <div key={index} onClick={()=>handleSendEmoji(emoji.src)}  className="cursor-pointer scale-90 hover:scale-110 transition-transform duration-100">
                         <Image src={emoji.src} alt={emoji.alt} width={35} height={35} />
                    </div>
                 )
